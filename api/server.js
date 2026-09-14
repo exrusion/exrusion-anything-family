953ced092a951cb966cbba0f36b930de77f58168
exrusion <97180105+exrusion@users.noreply.github.com>
2026-09-14 08:10:06 +0400

    Use official provider logos in api/server.js

--- api/server.js
+++ api/server.js
@@ -24,6 +24,7 @@ const staticFiles = new Map([
   ["/", ["index.html", "text/html; charset=utf-8"]],
   ["/index.html", ["index.html", "text/html; charset=utf-8"]],
   ["/styles.css", ["styles.css", "text/css; charset=utf-8"]],
+  ["/brand-logos.css", ["brand-logos.css", "text/css; charset=utf-8"]],
   ["/app.js", ["app.js", "text/javascript; charset=utf-8"]],
   ["/config.js", ["config.js", "text/javascript; charset=utf-8"]],
 ]);
